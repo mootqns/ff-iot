3f47344fb1719c2501297e16b61a2e81ff889704
matt nguyen <mqn360@gmail.com>
2025-03-27 16:30:06 -0700

    adding light strip

--- src/matt.cpp
+++ src/matt.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include <Audio.h>
 #include <SPIFFS.h>
-#include <Adafruit_SSD1306.h>
 #include <Wire.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_NeoPixel.h>
 
 // I2S pin configuration
 #define I2S_DOUT 5
@@ -16,8 +17,15 @@
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
+// Addressable LED Strip Pin
+#define LED_PIN  13
+#define NUM_LEDS 14
+
 // Create display object
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Create the NeoPixel LED strip object
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // File path in SPIFFS
 const char* mp3FilePath = "/boing.mp3";  
@@ -51,6 +59,17 @@ void setup() {
   // Display the text
   display.display();
 
+  // Initialize the LED strip
+  strip.begin();
+  strip.show();  
+  strip.setBrightness(100);  // Set brightness (0-255)
+  
+  // Set initial LED colors
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(0, 0, 255)); 
+  }
+  strip.show();
+
   // Initialize SPIFFS
   if (!SPIFFS.begin(true)) {
     Serial.println("SPIFFS Initialization Failed!");
@@ -61,7 +80,7 @@ void setup() {
   // Initialize I2S audio
   Serial.println("Initializing I2S...");
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-  audio.setVolume(10); // Set volume (0-21)
+  audio.setVolume(15); // Set volume (0-21)
 
   // Start MP3 playback from SPIFFS
   if (SPIFFS.exists(mp3FilePath)) {
@@ -74,7 +93,7 @@ void setup() {
 }
 
 void loop() {
-  audio.loop();  // Handles MP3 playback
+  audio.loop();  
 }
 
 // Debugging functions
